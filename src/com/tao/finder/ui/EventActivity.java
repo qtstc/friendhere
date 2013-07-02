4b95cac1acb8ef1ca2ddfb22e00b410ad02294b2
Tao Qian <taoqian_2015@depauw.edu>
2013-07-01 23:52:00 -0400

    Implemented background location update with boardcast receiver, not working.

--- src/com/tao/finder/ui/EventActivity.java
+++ src/com/tao/finder/ui/EventActivity.java
@@ -3,6 +3,12 @@ package com.tao.finder.ui;
 import java.util.List;
 import java.util.Locale;
 
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.parse.DeleteCallback;
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -11,15 +17,22 @@ import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.tao.finder.R;
+import com.tao.finder.logic.BackgroundLocationUpdater;
+import com.tao.finder.logic.LocationUtils;
 import com.tao.finder.logic.ParseContract;
 import com.tao.finder.logic.SuggestionProvider;
 import com.tao.finder.ui.SearchListFragment.OnSearchListener;
 
 import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Dialog;
 import android.app.FragmentTransaction;
+import android.app.PendingIntent;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
 import android.provider.SearchRecentSuggestions;
 import android.support.v4.app.Fragment;
@@ -38,8 +51,16 @@ import android.widget.SearchView;
 import android.widget.TextView;
 
 public class EventActivity extends FragmentActivity implements
-		ActionBar.TabListener,OnSearchListener {
+		ActionBar.TabListener,OnSearchListener, GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener{
 
+	 // A request to connect to Location Services
+    private LocationRequest mLocationRequest;
+
+    // Stores the current instantiation of the location client in this object
+    private LocationClient mLocationClient;
+	
+	
 	public static final String OBJECT_ID = "object_id";
 	ParseObject event;
 	ParseObject checkin;
@@ -65,8 +86,219 @@ public class EventActivity extends FragmentActivity implements
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_event);
 		handleIntent(getIntent());
+		initializeLocation();
+	}
+	
+	private void initializeLocation()
+	{
+		 // Create a new global location parameters object
+        mLocationRequest = LocationRequest.create();
+        /*
+         * Set the update interval
+         */
+        mLocationRequest.setInterval(LocationUtils.UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Use high accuracy
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Set the interval ceiling to one minute
+        mLocationRequest.setFastestInterval(LocationUtils.FAST_INTERVAL_CEILING_IN_MILLISECONDS);
+        /*
+         * Create a new location client, using the enclosing class to
+         * handle callbacks.
+         */
+        mLocationClient = new LocationClient(this, this, this);
 	}
 
+	@Override
+    public void onStop() {
+
+        // If the client is connected
+        if (mLocationClient.isConnected()) {
+            stopPeriodicUpdates();
+        }
+
+        // After disconnect() is called, the client is considered "dead".
+        mLocationClient.disconnect();
+
+        super.onStop();
+    }
+	
+	  /*
+     * Called when the Activity is restarted, even before it becomes visible.
+     */
+    @Override
+    public void onStart() {
+
+        super.onStart();
+
+        /*
+         * Connect the client. Don't re-start any requests here;
+         * instead, wait for onResume()
+         */
+        mLocationClient.connect();
+
+    }
+	
+	/*
+     * Handle results returned to this Activity by other Activities started with
+     * startActivityForResult(). In particular, the method onConnectionFailed() in
+     * LocationUpdateRemover and LocationUpdateRequester may call startResolutionForResult() to
+     * start an Activity that handles Google Play services problems. The result of this
+     * call returns here, to onActivityResult.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        // Choose what to do based on the request code
+        switch (requestCode) {
+
+            // If the request code matches the code sent in onConnectionFailed
+            case LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST :
+
+                switch (resultCode) {
+                    // If Google Play services resolved the problem
+                    case Activity.RESULT_OK:
+
+                        // Log the result
+                        Log.d(LocationUtils.APPTAG, getString(R.string.resolved));
+                    break;
+
+                    // If any other result was returned by Google Play services
+                    default:
+                        // Log the result
+                        Log.d(LocationUtils.APPTAG, getString(R.string.no_resolution));
+                    break;
+                }
+
+            // If any other request code was received
+            default:
+               // Report that this Activity received an unknown requestCode
+               Log.d(LocationUtils.APPTAG,
+                       getString(R.string.unknown_activity_request_code, requestCode));
+               break;
+        }
+    }
+    
+    /**
+     * Verify that Google Play services is available before making a request.
+     *
+     * @return true if Google Play services is available, otherwise false
+     */
+    private boolean servicesConnected() {
+
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d(LocationUtils.APPTAG, getString(R.string.play_services_available));
+
+            // Continue
+            return true;
+        // Google Play services was not available for some reason
+        } else {
+//            // Display an error dialog
+//            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
+//            if (dialog != null) {
+//                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+//                errorFragment.setDialog(dialog);
+//                errorFragment.show(getSupportFragmentManager(), LocationUtils.APPTAG);
+//            }
+            return false;
+        }
+    }
+    
+    /**
+     * Invoked by the "Get Location" button.
+     *
+     * Calls getLastLocation() to get the current location
+     *
+     * @param v The view object associated with this method, in this case a Button.
+     */
+    public void getLocation(View v) {
+
+        // If Google Play Services is available
+        if (servicesConnected()) {
+
+            // Get the current location
+            Location currentLocation = mLocationClient.getLastLocation();
+
+            // Display the current location in the UI
+            //mLatLng.setText(LocationUtils.getLatLng(this, currentLocation));
+        }
+    }
+    
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		  /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+                /*
+                * Thrown if Google Play services canceled the original
+                * PendingIntent
+                */
+
+            } catch (IntentSender.SendIntentException e) {
+
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+
+            // If no resolution is available, display a dialog to the user with the error.
+            //showErrorDialog(connectionResult.getErrorCode());
+        }
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		startPeriodicUpdates();
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+    
+    /**
+     * In response to a request to start updates, send a request
+     * to Location Services
+     */
+    private void startPeriodicUpdates() {
+    	Log.e("PendingAgent","started");
+        mLocationClient.requestLocationUpdates(mLocationRequest, getPendingIntent());
+    }
+    
+    
+    private PendingIntent getPendingIntent()
+    {
+    	Intent i = new Intent(getApplicationContext(),BackgroundLocationUpdater.class);
+    	return PendingIntent.getBroadcast(getApplicationContext(), 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+    
+    /**
+     * In response to a request to stop updates, send a request to
+     * Location Services
+     */
+    private void stopPeriodicUpdates() {
+        mLocationClient.removeLocationUpdates(getPendingIntent());
+    }
+    
 	private String getFragmentTag(int pos){
 	    return "android:switcher:"+R.id.pager+":"+pos;
 	}
@@ -143,6 +375,8 @@ public class EventActivity extends FragmentActivity implements
 						//TODO if exception caught, make checkin null.
 						item.setTitle(getString(R.string.action_check_out));
 						setProgressBarIndeterminateVisibility(false);
+						
+						startPeriodicUpdates();
 					}
 				});
 				return true;
@@ -155,6 +389,8 @@ public class EventActivity extends FragmentActivity implements
 					item.setTitle(getString(R.string.action_check_in));
 					setProgressBarIndeterminateVisibility(false);
 					checkin = null;
+					
+					stopPeriodicUpdates();
 				}
 			});
 			break;
